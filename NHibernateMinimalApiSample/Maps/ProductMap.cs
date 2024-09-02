625ba00e05af6bdd3dd60f00ff187dfa1674aa7c
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-09-02 21:39:09 +0630

    feat: create Product Map

---
+++ NHibernateMinimalApiSample/Maps/ProductMap.cs
@@ -0,0 +1,16 @@
+﻿using FluentNHibernate.Mapping;
+using NHibernateMinimalApiSample.Models;
+
+namespace NHibernateMinimalApiSample.Maps
+{
+    public class ProductMap : ClassMap<Product>
+    {
+        public ProductMap()
+        {
+            Table("Products");
+            Id(x => x.Id).GeneratedBy.Identity();
+            Map(x => x.Name).Not.Nullable();
+            Map(x => x.Price).Not.Nullable();
+        }
+    }
+}
