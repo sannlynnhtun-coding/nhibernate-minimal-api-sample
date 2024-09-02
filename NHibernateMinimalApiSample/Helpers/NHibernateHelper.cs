866d6093c168dc78aaab04b072cc3761c9fe3e8a
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-09-02 21:38:39 +0630

    feat: NHibernateHelper

---
+++ NHibernateMinimalApiSample/Helpers/NHibernateHelper.cs
@@ -0,0 +1,27 @@
+﻿using FluentNHibernate.Cfg.Db;
+using FluentNHibernate.Cfg;
+using NHibernate;
+using NHibernateMinimalApiSample.Models;
+using ISession = NHibernate.ISession;
+
+namespace NHibernateMinimalApiSample.Helpers
+{
+    public class NHibernateHelper
+    {
+        private readonly ISessionFactory _sessionFactory;
+
+        public NHibernateHelper(string connectionString)
+        {
+            var config = Fluently.Configure()
+                .Database(MsSqlConfiguration.MsSql2012.ConnectionString(connectionString))
+                .Mappings(m => m.FluentMappings.AddFromAssemblyOf<Product>())
+                .BuildConfiguration();
+
+            _sessionFactory = config.BuildSessionFactory();
+        }
+
+        public ISessionFactory SessionFactory => _sessionFactory;
+
+        public ISession OpenSession() => _sessionFactory.OpenSession();
+    }
+}
