be4f4121987629314112d0f8c0773d282c9862c1
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-09-02 21:38:57 +0630

    feat: create Product

---
+++ NHibernateMinimalApiSample/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace NHibernateMinimalApiSample.Models
+{
+    public class Product
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual decimal Price { get; set; }
+    }
+}
